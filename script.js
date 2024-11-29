8130c513e51d22104302e82b0f3d02c8ea3c8197
Maria Eduarda <dudaferres@yahoo.com>
2024-11-29 19:55:15 -0300

    att: link para a direção do login

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
         // Verifica qual tipo de usuário foi selecionado
         if (usuario === "paciente") {
             console.log("Login bem-sucedido como Paciente! Redirecionando...");
-            window.location.href = "Profissional/planoAlimentar1P.html";  // Redireciona para a página do paciente
+            window.location.href = "Profissional/planoAlimentarR.html";  // Redireciona para a página do paciente
         } else if (usuario === "profissional") {
             console.log("Login bem-sucedido como Profissional! Redirecionando...");
             window.location.href = "Profissional/planoAlimentar1P.html";  // Redireciona para a página do profissional
